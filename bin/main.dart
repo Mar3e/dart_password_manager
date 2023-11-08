84783d51793749313aecc2a44a35a10470e973e3
Mar3e <mar3e.1420.1435@gmail.com>
2023-11-08 13:27:46 +0300

    changed some confuguration

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,10 @@
 import 'package:args/command_runner.dart';
-import 'package:dart_password_manager/encryption.dart';
+import 'package:dart_password_manager/aes_algorithm.dart';
 
 void main(List<String> arguments) {
   // ignore: unused_local_variable
   final runner = CommandRunner(
       "dpassman", "A password manager that uses the dart language")
-    ..addCommand(Encryption())
+    ..addCommand(AesAlgorithm())
     ..run(arguments);
 }
---
+++ lib/storage.dart
@@ -0,0 +1,18 @@
+// this file is used to store all code sippets before I use them or understand them
+
+// used to create a folders (needs more work)
+//  homePath =
+//         Platform.environment['HOME'] ?? Platform.environment['USERPROFILE'];
+// void createFolder() {
+//     // Create a folder in the home directory
+//     appDirectory =
+//         Directory('$homePath/projects/dart_password_manager/testFolder');
+
+//     // Create the folder if it doesn't already exist
+//     if (!appDirectory.existsSync()) {
+//       appDirectory.create();
+//       stdout.write("Folder created successfully!");
+//     } else {
+//       stdout.write('Folder already exists.${appDirectory.path}');
+//     }
+//   }
