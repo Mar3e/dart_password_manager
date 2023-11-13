30c6a3e4f678116e22bc1ba081712719fc807bc1
Mar3e <mar3e.1420.1435@gmail.com>
2023-11-13 11:17:32 +0300

    finnshed the basics of set command

--- lib/set_command.dart
+++ lib/set_command.dart
@@ -6,6 +6,7 @@ import 'package:dart_password_manager/aes_algorithm.dart';
 import 'package:dart_password_manager/settings.dart';
 
 class SetCommand extends Command {
+  Map<String, dynamic> passwordDetails = {};
   @override
   String get name => "set";
   @override
@@ -13,16 +14,25 @@ class SetCommand extends Command {
 
   @override
   FutureOr? run() {
-    askForMasterKey();
-    getPasswordName();
+    String masterKey = askForMasterKey();
+    String passwordName = getPasswordName();
+    getPasswordUserName();
+    getThePassword();
+    getExtraDetails();
+    savePassWord(
+      masterKey: masterKey,
+      passwordName: passwordName,
+      passwordDetails: passwordDetails,
+    );
   }
 
-  void askForMasterKey() {
+  String askForMasterKey() {
     stdout.write("Please enter your master key: ");
     String? masterKey = stdin.readLineSync();
     if (masterKey != null && masterKey.isNotEmpty) {
       String hashedMasterKey = AesAlgorithm.hashKey(masterKey);
       checkMasterKey(hashedMasterKey);
+      return masterKey;
     } else {
       stdout.write("Please enter something!!");
       exit(2);
@@ -41,13 +51,68 @@ class SetCommand extends Command {
     }
   }
 
-  void getPasswordName() {
+  String getPasswordName() {
     stdout.write("Please enter the name of the password: ");
-    final passwordName = stdin.readLineSync();
-    if (passwordName == null || passwordName.isEmpty) {
+    final passWordName = stdin.readLineSync();
+    if (passWordName == null || passWordName.isEmpty) {
+      stdout.writeln("You did not enter anything!!");
+      exit(2);
+    } else {
+      return passWordName;
+    }
+  }
+
+  void getPasswordUserName() {
+    stdout.write(
+        "Please enter the userName or Email associated with the password: ");
+    final passWordUserName = stdin.readLineSync();
+    if (passWordUserName == null || passWordUserName.isEmpty) {
+      stdout.writeln("You did not enter anything!!");
+      exit(2);
+    } else {
+      passwordDetails["passWordUserName"] = passWordUserName;
+    }
+  }
+
+  void getThePassword() {
+    stdout.write("Please enter the password: ");
+    final passWord = stdin.readLineSync();
+    if (passWord == null || passWord.isEmpty) {
       stdout.writeln("You did not enter anything!!");
       exit(2);
     }
-    print(passwordName);
+    stdout.write("Please re enter the password: ");
+    final rePassWord = stdin.readLineSync();
+    if (rePassWord == null || rePassWord.isEmpty) {
+      stdout.writeln("You did not enter anything!!");
+      exit(2);
+    } else if (passWord != rePassWord) {
+      stdout.writeln("The password did not match!!");
+      exit(2);
+    } else {
+      passwordDetails["passWord"] = passWord;
+    }
+  }
+
+  void getExtraDetails() {
+    stdout
+        .write("Please enter the URL associated with the password(Optional): ");
+    final passWordUrl = stdin.readLineSync();
+    passwordDetails["passWordUrl"] = passWordUrl ?? "";
+
+    stdout.write("Please enter the discretion of password(Optional): ");
+    final passWordDescription = stdin.readLineSync();
+    passwordDetails["passWordDescription"] = passWordDescription ?? "";
+  }
+
+  void savePassWord({
+    required String masterKey,
+    required String passwordName,
+    required Map<String, dynamic> passwordDetails,
+  }) {
+    AesAlgorithm.encrypt(
+        masterPassWord: masterKey,
+        passwordFileName: passwordName,
+        passwordFileDetails: passwordDetails);
   }
 }
