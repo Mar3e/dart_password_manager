f64634c3b36d3be52f2735aa0f008c981b3206cc
Mar3e <mar3e.1420.1435@gmail.com>
2023-11-16 13:08:33 +0300

    added cryptohraohy testing

---
+++ lib/cryptography.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:aes_crypt_null_safe/aes_crypt_null_safe.dart';
+import 'package:dart_password_manager/file_manager.dart';
+import 'package:dart_password_manager/settings.dart';
+import 'package:pointycastle/pointycastle.dart';
+
+class Cryptography {
+  static void encrypt({
+    required String masterPassWord,
+    required String passwordName,
+    required Map<String, dynamic> passwordDetails,
+  }) {
+    Uint8List key = _generateKey(Settings.settings["salt"], masterPassWord);
+    String keyAsString = base64.encode(key);
+    final crypt = AesCrypt(keyAsString);
+    crypt.setOverwriteMode(AesCryptOwMode.warn);
+
+    try {
+      String passwordDetailsStr = json.encode(passwordDetails);
+      crypt.encryptTextToFileSync(passwordDetailsStr,
+          "${FileManager.homePath}/.dpassman/$passwordName.txt.aes");
+    } on AesCryptException {
+      throw Exception("The password is already exists");
+    }
+  }
+
+  static Map<String, dynamic> decrypt({
+    required String masterPassWord,
+    required String passWordName,
+  }) {
+    Uint8List key = _generateKey(Settings.settings["salt"], masterPassWord);
+    String keyAsString = base64.encode(key);
+    final crypt = AesCrypt(keyAsString);
+
+    final decryptedString = crypt.decryptTextFromFileSync(
+        "${FileManager.homePath}/.dpassman/$passWordName.txt.aes");
+
+    Map<String, dynamic> decryptedMap = json.decode(decryptedString);
+
+    return decryptedMap;
+  }
+
+  static Uint8List _generateKey(String salt, String masterPassWord) {
+    Uint8List passphrase = Uint8List.fromList(utf8.encode(masterPassWord));
+    Uint8List salt = Uint8List.fromList(utf8.encode(Settings.settings["salt"]));
+
+    var derivator = KeyDerivator(Settings.settings["pbkdf2Name"]);
+    var params = Pbkdf2Parameters(salt, Settings.settings["pbkdf2Iterations"],
+        Settings.settings["algorithmKeySize"] ~/ 8);
+    derivator.init(params);
+
+    return derivator.process(passphrase);
+  }
+
+  static String hashString(String key) {
+    Uint8List keyBytes = Uint8List.fromList(utf8.encode(key));
+
+    Digest sha256 = Digest('SHA-256');
+
+    Uint8List hashedBytes = sha256.process(keyBytes);
+
+    String hashedKey = base64.encode(hashedBytes);
+
+    return hashedKey;
+  }
+}
---
+++ test/cryptography_test.dart
@@ -0,0 +1,45 @@
+import 'package:dart_password_manager/cryptography.dart';
+import 'package:dart_password_manager/file_manager.dart';
+import 'package:test/test.dart';
+
+void main() {
+  String passwordName = "test";
+  Map<String, dynamic> passwordDetails = {
+    'passWordUserName': 'testUser',
+    'passWord': '1234',
+    'passWordUrl': '',
+    'passWordDescription': 'test@test.com'
+  };
+  group('Encryption tests', () {
+    //! note: before you run this test you need to check if the file already exits or it will fail, I will add later a function to delete a file.
+    test(
+        'When entering a correct details, we should find a file created as the destination and if tried to add another password with the same name an exception thrown',
+        () {
+      Cryptography.encrypt(
+          masterPassWord: "mar",
+          passwordName: passwordName,
+          passwordDetails: passwordDetails);
+      final file = FileManager.findFile("$passwordName.txt.aes");
+
+      expect(file?.path.split("/").last, "test.txt.aes");
+// If called again throw an exception
+      expect(
+          () => Cryptography.encrypt(
+              masterPassWord: "mar",
+              passwordName: passwordName,
+              passwordDetails: passwordDetails),
+          throwsA(isException));
+    });
+  });
+
+  group("Decryption tests", () {
+    test(
+        'When entering the correct file name, we should get the password details as a string',
+        () {
+      final decryptedPassWordDetails = Cryptography.decrypt(
+          masterPassWord: "mar", passWordName: passwordName);
+
+      expect(decryptedPassWordDetails, passwordDetails);
+    });
+  });
+}
